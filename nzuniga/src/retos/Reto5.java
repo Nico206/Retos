68eaeec67c418f8bf83196c354575529034ca2bf
Nicolas <nicolas.music2006@gmail.com>
2023-03-01 11:52:01 -0500

    RETOS TARDE

--- nzuniga/src/retos/Reto5.java
+++ nzuniga/src/retos/Reto5.java
@@ -4,7 +4,24 @@ import java.util.Scanner;
 public class Reto5 {
     public static void main(String[] args) {
         Scanner lectura= new Scanner(System.in);
-          
+              String nombre,apellido, nameComplete;
+
+              System.out.println("por favor digite su nombre");
+              nombre= lectura.nextLine();
+              System.out.println(nombre.length());
+              System.out.println("por favor digite su apellido");
+              apellido= lectura.nextLine();
+              System.out.println(apellido.length());
+
+              System.out.println(nombre.toUpperCase());
+              System.out.println(apellido.toLowerCase());
+              nameComplete = nombre + apellido;           
+
+
+
+
+
+
          lectura.close();
     }
 }
